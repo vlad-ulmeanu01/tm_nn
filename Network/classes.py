30d1e13d089b6331dcdf555d74ae9b28569767b4
vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
2023-09-03 17:43:28 +0300

    Bugfix Network

--- Network/classes.py
+++ Network/classes.py
@@ -37,7 +37,7 @@ class MainNet(torch.nn.Module):
         self.sharedNet = SharedNet()
 
         self.stretchSigmoid = lambda x: torch.sigmoid(x / 4)  # torch.nn.Sigmoid()
-        self.softmax = torch.nn.Softmax(dim = 1)
+        self.softmax = torch.nn.Softmax(dim = -1)
 
         self.fc1_gas = torch.nn.Linear(200, LEN_OUTPUT_GAS)
         self.fc1_brake = torch.nn.Linear(200, LEN_OUTPUT_BRAKE)
