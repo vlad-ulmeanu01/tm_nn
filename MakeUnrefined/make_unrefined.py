3748cb5a2d0e7dec5b78cb262c15b0b0a59e74d5
vlad-ulmeanu01 <vlad.ulmeanu01@gmail.com>
2023-09-03 19:45:39 +0300

    MakeUnrefined 3 taste.

--- MakeUnrefined/make_unrefined.py
+++ MakeUnrefined/make_unrefined.py
@@ -1,25 +1,31 @@
 import pandas as pd
+import sys
 import os
 
 import make_input_from_csv_pair
 
-cnt, finalCnt = 0, 581
-skip = ["/", "Dirt-0"]
+cnt, finalCnt = 0, 456
+l, r = map(int, sys.argv[1:])
 
-for path, subdirs, files in os.walk("/home/vlad/Desktop/Probleme/Trackmania/RawDataset/Others/"):
-    ok = True
-    for s in skip:
-        if path.endswith(s):
-            ok = False
+for dir in ["/home/vlad/Desktop/Probleme/Trackmania/RawDataset/Others/RL-Train1/", "/home/vlad/Desktop/Probleme/Trackmania/RawDataset/Pro/"]:
+    for path, subdirs, files in os.walk(dir):
+        # ok = True
+        # for s in ["/", "Dirt-0"]:
+        #     if path.endswith(s):
+        #         ok = False
 
-    if ok:
-        for i in range(len(files)):
-            fPath1 = os.path.join(path, files[i])
-            if pd.read_csv(fPath1, skipinitialspace = True)["steer"].nunique() > 3:
-                for j in range(len(files)):
-                    if j != i:
-                        fPath2 = os.path.join(path, files[j])
-                        fName = make_input_from_csv_pair.make_input_from_pair(fPath1, fPath2)
+        ok = (not path.endswith('/') or path.endswith("RL-Train1/"))
 
-                        cnt += 1
-                        print(f"ok {cnt}/{finalCnt}: {fPath1}, {fPath2}, {fName}.")
+        if ok:
+            for i in range(len(files)):
+                fPath1 = os.path.join(path, files[i])
+                if pd.read_csv(fPath1, skipinitialspace = True)["steer"].nunique() <= 3:
+                    for j in range(len(files)):
+                        if j != i:
+                            cnt += 1
+
+                            if l <= cnt <= r:
+                                fPath2 = os.path.join(path, files[j])
+                                fName = make_input_from_csv_pair.make_input_from_pair(fPath1, fPath2)
+
+                                print(f"ok {cnt}/{finalCnt}: {fPath1}, {fPath2}, {fName}.")
